b20b81c61fea73f4526a5fac4da8d45e11c76ec4
Teddy N'go <teddy@teamtangible.com>
2022-05-18 09:46:18 -0400

    Modified README and tweaked some things

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 import "./styles/styles.css";
 import "./styles/loader.css";
 import App from "./App";
-import Test from "./Tests";
 
 ReactDOM.render(
     <React.StrictMode>
         <App />
-        {/*<Test/>*/}
     </React.StrictMode>,
     document.getElementById("root")
 );
